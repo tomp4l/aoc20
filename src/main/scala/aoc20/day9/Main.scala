9711c77d0d26e057afb6e39ec92f79cbe2bab6bf
Tom Mottram <tom@pay4later.com>
2020-12-09 09:32:11 +0000

    Day 9

---
+++ src/main/scala/aoc20/day9/Main.scala
@@ -0,0 +1,39 @@
+package aoc20
+package day9
+
+import cats.effect.IOApp
+import cats.effect.{ExitCode, IO}
+
+object Main extends IOApp {
+
+  override def run(args: List[String]): IO[ExitCode] =
+    readInputLongs(9)
+      .flatMap { numbers =>
+        val invalidNumber = findFirstInvalid(numbers)
+        for {
+          _ <- Console.outputOptional(1, invalidNumber)
+          _ <- Console.outputOptional(
+            2,
+            invalidNumber.flatMap(findContiguous(numbers)),
+          )
+        } yield ()
+      }
+      .as(ExitCode.Success)
+
+  def findFirstInvalid(numbers: Vector[Long]): Option[Long] =
+    numbers
+      .sliding(26)
+      .find { slice =>
+        val search = slice.init
+        val target = slice.last
+        val s = search.toSet
+        !search.map(v => target - v).exists(s.contains(_))
+      }
+      .map(_.last)
+
+  def findContiguous(numbers: Vector[Long])(target: Long): Option[Long] =
+    numbers.tails
+      .flatMap(_.inits.find(_.sum == target))
+      .find(_.nonEmpty)
+      .map(v => v.min + v.max)
+}
